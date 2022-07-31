ea82aca17560725458b95dc2afd999ddf4e9e91b
NoobSaiyan <ms19961998@gmail.com>
2022-07-31 17:11:53 +0530

    asynchronous tests

---
+++ async.test.js
@@ -0,0 +1,26 @@
+const { sumAsync, subtractAsync } = require('./math')
+
+// Promises
+test('promise returns sum in async', () => {
+  return sumAsync(2, 2).then(data => {
+    expect(data).toBe(4)
+  })
+})
+
+// Async/Await
+test('async/await returns sum in async', async () => {
+  const data = await sumAsync(2, 2)
+  expect(data).toBe(4)
+})
+test('returns sum in async', async () => {
+  const data = await sumAsync(2, 2)
+  expect(data).toBe(4)
+})
+
+// Resolve/Reject
+test('sum resolve', () => {
+  return expect(sumAsync(2, 2)).resolves.toBe(4)
+})
+// test('sum reject', () => {
+//   return expect(sumAsync(2, 2)).rejects.toMatch('error')
+// })
